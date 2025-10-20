4221c04a225e8a83c22f0616a4d03ec3134d2a40
Devendra Kumar Narware <devn2405@gmail.com>
2025-10-20 14:54:38 +0530

    Time: 1 ms (99.33%) | Memory: 42 MB (48.39%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,12 +1,11 @@
 class Solution {
     public char findTheDifference(String x, String y) {
        char ans = 0 ;
-       for(int i = 0 ; i<x.length() ; i++){
-           ans = (char)(ans^x.charAt(i) );
-       }
-       for(int i = 0 ; i<y.length() ; i++){
-           ans = (char)(ans^y.charAt(i) );
-       }
-       return ans ;
+        for(char i : x.toCharArray()){
+            ans^=i;
+        }
+        for(char i : y.toCharArray()){
+            ans^=i;        }
+        return ans;
     }
 }
