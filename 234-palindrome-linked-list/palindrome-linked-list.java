447d85022aaf36a5eeeb70a367b43827395df9c8
Devendra Kumar Narware <devn2405@gmail.com>
2025-10-29 21:00:11 +0530

    Time: 4 ms (82.71%) | Memory: 69.2 MB (13.84%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,49 +10,28 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        int size = 0 ;
-        ListNode temp = head ;
-        ListNode rightHead ;
 
-        //FINDING SIZE
+        ListNode slow = head ;
+        ListNode fast = head ;
 
-        while(temp != null){
-            size++ ;
-            temp = temp.next ;
+        //FINDING MID
+        while(fast != null && fast.next != null){
+            slow = slow.next ;
+            fast = fast.next.next ;
         }
-        if(size == 1){
-            return true ;
-        }
-        //FINDING THE NODE BEFORE MID
-        temp = head ;
-        int mid = size/2 ;
-        for(int i = 0 ; i < mid - 1 ; i++){
-            temp = temp.next ;
-        }
-
-        //BREAKING THE NODE FROM THE MID ACC TO EVEN-ODD
-
-        if(size % 2 == 0 ){
-            rightHead = temp.next ;
-            temp.next = null ;
-        }else{
-            rightHead = temp.next.next ;
-            temp.next = null ;
-        }
-
-
+       
         //REVERSE THE RIGHT PART OF THER NODE
 
-        rightHead = reverse(rightHead) ;
+        ListNode right = reverse(slow) ;
 
 
         //CHECKING IF PALINDROME OR NOT
 
-        while(head != null){
-            if(head.val != rightHead.val){
+        while(right != null){
+            if(head.val != right.val){
                 return false ;
             }
-            rightHead = rightHead.next ;
+            right = right.next ;
             head = head.next ;
         } 
         return true ;
