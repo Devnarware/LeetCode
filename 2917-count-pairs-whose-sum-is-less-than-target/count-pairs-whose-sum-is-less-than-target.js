9fca8b5e408919a09cc0f6851dfceabed09bb6ce
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-16 15:56:08 +0530

    Time: 2 ms (33.33%) | Memory: 55.7 MB (72.11%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    nums.sort((a,b) => a - b) ;
+
+    let i = 0 ;
+    let j = nums.length -1 ;
+    let count = 0 ;
+
+    while(i < j){
+
+        if(nums[i] + nums[j] < target){
+            count += (j-i) ;
+            i++ ;
+        }else{
+            j-- ;
+        }
+    }
+
+    return count ;
+};
