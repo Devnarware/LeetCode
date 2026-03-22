b295d4ef7c1cd36142f20f853edf8b4918b9c530
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-23 00:20:23 +0530

    Time: 1 ms (51.23%) | Memory: 5.8 MB (62.04%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,14 +1,14 @@
 func twoSum(nums []int, target int) []int {
     n := len(nums)
-    arr := []int{0,0}
+    hmap := make(map[int] int)
     for i := 0 ; i<n ; i++{
-        for j := i+1 ; j<n ; j++{
-            if nums[i]+nums[j] == target{
-                arr[0] = i
-                arr[1] = j
-                return arr
-            }
+        lookingFor := target - nums[i] ;
+        index, found := hmap[lookingFor]
+        if found{
+            return []int{i, index} ;
         }
+
+        hmap[nums[i]] = i ;
     }
-    return arr
+    return []int{}
 }
