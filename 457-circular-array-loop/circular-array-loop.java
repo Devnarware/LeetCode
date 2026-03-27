c1259d4e8a6af5075e67336824b667075b32b990
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-27 21:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (97.12%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.java
+++ 457-circular-array-loop/circular-array-loop.java
@@ -22,60 +22,46 @@ class Solution {
             if(nums[i] == 0){
                 continue ;
             }
-
-            // set :- to check that if we visit on that index or not
-            Set<Integer> set = new HashSet<>() ;
-            set.add(i) ; // already there so marked as visited
-
-            // a flaf(isPos) -> shows that the vurr element is +ve or -ve
             boolean isPos = nums[i] > 0 ;
-            int curr = i ;
-            // for checking the loop 
-            while(true){
 
-                // get for next idx
-                int next = nextIdx(nums, curr) ;
+            int fast = i ;
+            int slow = i ;
 
-                // checking for curr idx isPos or not
+            do{
+                slow = nextIdx(nums, slow) ;
+
+                fast = nextIdx(nums, fast) ;
                 if(isPos){
-                    if(nums[next] < 0){
-                        // coz our old num was pos
+                    if(nums[fast] < 0){
                         break ;
-                    }else{
-                        if(set.contains(next)){// checking already visit or not, means finding cycle
-
-                            // have to check is k>1 (isSelf loop)
-                            if(curr != next){
-                                return true ;
-                            }else{
-                                break ;
-                                // coz we are stuck in the self loop
-                            }
-                        }
-
-                        set.add(next) ;
                     }
-                }else {
-                    if(nums[next] > 0){
-                        // coz our last num was neg
+                }else{
+                    if(nums[fast] > 0){
                         break ;
-                    }else{
-                        if(set.contains(next)){// checking already visit or not, means finding cycle
-
-                            // have to check is k>1 (isSelf loop)
-                            if(curr != next){
-                                return true ;
-                            }else{
-                                break ; // self loop
-                            }
-                        }
-                        set.add(next) ;
                     }
                 }
 
-                curr = next ;
-            }
-            curr = i ;
+                fast = nextIdx(nums, fast) ;
+
+                if(isPos){
+                    if(nums[fast] < 0){
+                        break ;
+                    }
+                }else{
+                    if(nums[fast] > 0){
+                        break ;
+                    }
+                }
+
+                if( slow == fast){
+                    if(slow != nextIdx(nums, slow) ){
+                        return true ;
+                    }
+                    break ;
+                }
+            }while(slow != fast) ;
+            
+            int curr = i ;
             if(isPos){
                 while(nums[curr] > 0){
                     int next = nextIdx(nums, curr) ;
